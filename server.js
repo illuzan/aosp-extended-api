8499f5ef69e709f897daed61959cd0739fcabfb2
Gaurav Singh <allstargaurav@gmail.com>
2022-01-03 14:48:39 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,19 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
