7cb5ec8fc51a8b5b61331a24f1806d8748df14f5
Gaurav Singh <allstargaurav@gmail.com>
2022-01-05 19:45:15 +0530

    Add all constants

---
+++ constants.js
@@ -0,0 +1,35 @@
+const newsUrl = 'https://t.me/aospextended'
+const websiteUrl = 'https://aospextended.com'
+const APIhost = 'https://api.aospextended.com/'
+const donateUrl = 'http://paypal.me/ishubhamsingh'
+const Clienthost = 'https://downloads.aospextended.com/'
+const devicesJson =
+  'https://raw.githubusercontent.com/AospExtended/official_devices/'
+const buildsJson =
+  'https://raw.githubusercontent.com/AospExtended-Devices/official_builds/'
+const devicesBranch = 'main'
+const buildsBranch = 'master'
+const cacheInterval = 1800
+const botInterval = 300000
+
+const buildsDir = '/Users/gaurav/aexbuilds/builds/'
+const addonsDir = '/home/aexbuilds/addons/'
+
+const pushOperation = 'push'
+
+export {
+  buildsJson,
+  buildsBranch,
+  cacheInterval,
+  APIhost,
+  buildsDir,
+  devicesJson,
+  devicesBranch,
+  pushOperation,
+  donateUrl,
+  websiteUrl,
+  newsUrl,
+  addonsDir,
+  Clienthost,
+  botInterval,
+}
