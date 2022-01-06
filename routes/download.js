fedd503adaadc952a1ab60b794fce9f9bc8cdbbd
Gaurav Singh <allstargaurav@gmail.com>
2022-01-06 13:26:07 +0530

    Fix download issue

--- routes/download.js
+++ routes/download.js
@@ -8,54 +8,50 @@ export default async function download(fastify, options) {
   // @route  GET download/:codename/:androidVersion/:buildNameHex
   // @desc   Redirects to download of particular build of particular device post nougat android version
   // @access Public
-  fastify.get(
-    '/:codename/:androidVersion/:buildNameHex',
-    async (request, reply) => {
-      const { codename, androidVersion, buildNameHex } = request.params
-      const remoteip =
-        request.headers['x-forwarded-for'] ||
-        request.connection.remoteAddress ||
-        request.socket.remoteAddress ||
-        (request.connection.socket
-          ? request.connection.socket.remoteAddress
-          : null)
-      const buildName = decrypt(buildNameHex)
-      try {
-        if (
-          existsSync(
-            buildsDir + '/' + codename + '/' + androidVersion + '/' + buildName
-          )
-        ) {
-          reply.download(
-            '/' + codename + '/' + androidVersion + '/' + buildName
-          )
+  fastify.get('/:codename/:androidVersion/*', async (request, reply) => {
+    const { codename, androidVersion } = request.params
+    let buildNameHex = request.params['*']
+    const remoteip =
+      request.headers['x-forwarded-for'] ||
+      // request.connection.remoteAddress ||
+      request.socket.remoteAddress ||
+      (request.connection.socket
+        ? request.connection.socket.remoteAddress
+        : null)
+    const buildName = decrypt(buildNameHex)
+    try {
+      if (
+        existsSync(
+          buildsDir + '/' + codename + '/' + androidVersion + '/' + buildName
+        )
+      ) {
+        reply.download('/' + codename + '/' + androidVersion + '/' + buildName)
 
-          if (buildName.includes('.zip')) {
-            // Don't count download for recovery
-            const stats = new DownloadStats({
+        if (buildName.includes('.zip')) {
+          // Don't count download for recovery
+          const stats = new DownloadStats({
+            buildName,
+            userIp: remoteip,
+            downloadedAt: Math.floor(Date.now()),
+          })
+          try {
+            const count = await DownloadStats.countDocuments({
               buildName,
               userIp: remoteip,
-              downloadedAt: Math.floor(Date.now()),
             })
-            try {
-              const count = await DownloadStats.countDocuments({
-                buildName,
-                userIp: remoteip,
-              })
-              if (count === 0) {
-                stats.save()
-              }
-            } catch (error) {
-              console.error(error)
+            if (count === 0) {
+              stats.save()
             }
+          } catch (error) {
+            console.error(error)
           }
-        } else {
-          reply.code('403').send({ status: 'Not found' })
         }
-      } catch (error) {
-        console.log(error)
+      } else {
         reply.code('403').send({ status: 'Not found' })
       }
+    } catch (error) {
+      console.log(error)
+      reply.code('403').send({ status: 'Not found' })
     }
-  )
+  })
 }
